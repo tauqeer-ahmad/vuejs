aa813476d49619c76bde8d5bc26b219ba40dc386
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-01-31 14:14:56 +0500

    assignment 5 files added

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            myName: "Yasir",
-            myAge: 24,
-            imageUrl: "https://xprolabs.com/assets/logo-56ba1c0eb664802e32a79198fd8a8e2c83dba2c2fada2b0c4b2389e43321e577.svg"
+            taskInput: '',
+            tasks: [],
+            showList: true
+        }
+    },
+    computed: {
+        toggleBtnCaption() {
+            return this.showList ? 'Hide List' : 'Show List';
         }
     },
     methods: {
-        showRandom(){
-            return Math.random();
+        addTask() {
+            this.tasks.push(this.taskInput);
         },
-        showName(){
-            return this.myName;
+        toggleList() {
+            this.showList = !this.showList;
         }
     }
 });
