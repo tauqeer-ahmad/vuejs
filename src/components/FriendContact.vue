cf261ef4baf8cc735e1640820375f28a00cb3344
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-02-06 17:12:38 +0500

    introducing components in vue

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails">{{ detailsAreVisible? 'Hide': 'Show' }} Details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 'yasir',
+                name: 'yasir mughal',
+                phone: '1234 5678 90',
+                email: 'yasir@gmail.com'
+            }
+        }
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+};
+</script>
