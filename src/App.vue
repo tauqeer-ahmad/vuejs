7d0a748d27595c4ea13e09e36e3f80d594cbd9da
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-02-10 17:00:52 +0500

    assignment 6 | components

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <section>
+        <active-user :username="user.name" :userage="user.age"></active-user>
+        <user-data @user-data="updateUserData"></user-data>
+    </section>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            user: {
+                name: 'Yasir',
+                age: 24
+            }
+        }
+    },
+    methods: {
+        updateUserData(name, age) {
+            this.user.name = name;
+            this.user.age = age;
+        }
+    }
+};
+</script>
---
+++ src/components/UserData.vue
@@ -0,0 +1,29 @@
+<template>
+    <section>
+        <div>
+            <label>Username</label>
+            <input type="text" v-model="username">
+        </div>
+        <div>
+            <label>Age</label>
+            <input type="number" v-model="age">
+        </div>
+        <button @click="setUserData">Set User Data</button>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            age: ''
+        }
+    },
+    methods: {
+        setUserData() {
+            this.$emit('user-data', this.username, this.age);
+        }
+    }
+};
+</script>
