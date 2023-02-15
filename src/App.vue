ffcaa9cd526c3033a0f58c2a55e21b193b1711a6
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-02-15 13:37:06 +0500

    Setup & First steps

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <ul>
+        <li v-for="resource in storedResources" :key="resource.id">{{ resource }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'official-guide',
+                    title: 'Official Guide',
+                    description: 'The official vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn how to use googel',
+                    link: 'https://google.com'
+                }
+            ]
+        }
+    }
+}
+</script>
