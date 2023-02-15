04f83ea8c41533164d167011bea4d9a8d314c643
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-02-15 15:05:47 +0500

    Header & BaseCard Components added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-    <stored-resources :resources="storedResources"></stored-resources>
+    <div>
+        <the-header title="RememberMe"></the-header>
+        <stored-resources :resources="storedResources"></stored-resources>
+    </div>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
     components: {
-        'stored-resources': StoredResources
+        'stored-resources': StoredResources,
+        'the-header': TheHeader
     },
     data() {
         return {
--- src/components/learning-resources/LearningResoure.vue
+++ src/components/learning-resources/LearningResoure.vue
@@ -1,23 +1,23 @@
 <template>
-    <li>
-        <div>
-            <header>
-                <h3>{{ title }}</h3>
-                <button>Delete</button>
-            </header>
-        </div>
-        <p>{{ description }}</p>
-        <nav>
-            <a :href="link">
-                View Resource
-            </a>
-        </nav>
-    </li>
+  <li>
+    <base-card>
+      <header>
+        <h3>{{ title }}</h3>
+        <button>Delete</button>
+      </header>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">
+          View Resource
+        </a>
+      </nav>
+    </base-card>
+  </li>
 </template>
 
 <script>
 export default {
-    props: ['title', 'description', 'link']
+  props: ['title', 'description', 'link']
 }
 </script>
 
