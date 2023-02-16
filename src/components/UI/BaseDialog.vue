05232ac3327802ee1da37129798a8e285dc9801c
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-02-16 13:26:33 +0500

    teleport added

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -1,21 +1,23 @@
 <template>
-    <div @click="$emit('close')">
-        <dialog open>
-            <header>
-                <slot name="header">
-                    <h2>{{ title }}</h2>
-                </slot>
-            </header>
-            <section>
-                <slot></slot>
-            </section>
-            <menu>
-                <slot name="actions">
-                    <base-button @click="$emit('close')">Close</base-button>
-                </slot>
-            </menu>
-        </dialog>
-    </div>
+    <teleport to="body">
+        <div @click="$emit('close')">
+            <dialog open>
+                <header>
+                    <slot name="header">
+                        <h2>{{ title }}</h2>
+                    </slot>
+                </header>
+                <section>
+                    <slot></slot>
+                </section>
+                <menu>
+                    <slot name="actions">
+                        <base-button @click="$emit('close')">Close</base-button>
+                    </slot>
+                </menu>
+            </dialog>
+        </div>
+    </teleport>
 </template>
 
 <script>
