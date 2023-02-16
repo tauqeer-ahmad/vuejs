e5e41d3cfa8ea4cadda9c9ccf335f21304e6ea54
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-02-16 12:40:35 +0500

    modal dialog added

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,34 +1,57 @@
 <template>
-    <base-card>
-        <form @submit.prevent="submitData">
-            <div class="form-control">
-                <label for="title">Title</label>
-                <input id="title" name="title" type="text" ref="titleInput" />
-            </div>
-            <div class="form-control">
-                <label for="description">Description</label>
-                <textarea id="description" name="description" rows="3" ref="descInput"></textarea>
-            </div>
-            <div class="form-control">
-                <label for="link">Link</label>
-                <input id="link" name="link" type="url" ref="linkInput" />
-            </div>
-            <div>
-                <base-button type="submit">Add Resource</base-button>
-            </div>
-        </form>
-    </base-card>
+    <div>
+        <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+            <template #default>
+                <p>Unfortunately, at least one input value is Invalid.</p>
+                <p>Please check all inputs</p>
+            </template>
+            <template #actions>
+                <base-button @click="confirmError">Okay</base-button>
+            </template>
+        </base-dialog>
+        <base-card>
+            <form @submit.prevent="submitData">
+                <div class="form-control">
+                    <label for="title">Title</label>
+                    <input id="title" name="title" type="text" ref="titleInput" />
+                </div>
+                <div class="form-control">
+                    <label for="description">Description</label>
+                    <textarea id="description" name="description" rows="3" ref="descInput"></textarea>
+                </div>
+                <div class="form-control">
+                    <label for="link">Link</label>
+                    <input id="link" name="link" type="url" ref="linkInput" />
+                </div>
+                <div>
+                    <base-button type="submit">Add Resource</base-button>
+                </div>
+            </form>
+        </base-card>
+    </div>
 </template>
 
 <script>
 export default {
     inject: ['addResource'],
+    data() {
+        return {
+            inputIsInvalid: false
+        }
+    },
     methods: {
         submitData() {
             const enteredTitle = this.$refs.titleInput.value;
             const enteredDesc = this.$refs.descInput.value;
             const enteredLink = this.$refs.linkInput.value;
+            if (enteredTitle.trim() === '' || enteredDesc.trim() === '' || enteredLink.trim() === '') {
+                this.inputIsInvalid = true;
+                return;
+            }
             this.addResource(enteredTitle, enteredDesc, enteredLink);
+        },
+        confirmError() {
+            this.inputIsInvalid = false;
         }
     }
 }
