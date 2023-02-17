5386dc4cf862840fb19d2cdfb999951f26699913
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-02-17 18:22:54 +0500

    showing loading message

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -5,7 +5,8 @@
       <div>
         <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-else>
         <survey-result v-for="result in results" :key="result.id" :name="result.name"
           :rating="result.rating"></survey-result>
       </ul>
@@ -22,16 +23,19 @@ export default {
   },
   data() {
     return {
-      results: []
+      results: [],
+      isLoading: false,
     }
   },
   methods: {
     loadExperiences() {
+      this.isLoading = true;
       fetch('https://vuejs-demo-http-8daaa-default-rtdb.firebaseio.com/.json').then((response) => {
         if (response.ok) {
           return response.json();
         }
       }).then((data) => {
+        this.isLoading = false;
         const results = [];
         for (const id in data) {
           results.push({
