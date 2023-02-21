007d169f547e64b89a011eead5daff25a4ba91a9
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-02-21 14:49:21 +0500

    Rendering Multiple Routes with Named Router Views

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <the-navigation @set-page="setActivePage"></the-navigation>
+  <the-navigation></the-navigation>
   <main>
     <router-view></router-view>
   </main>
+  <footer>
+    <router-view name="footer"></router-view>
+  </footer>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue'
 import NotFound from './components/nav/NotFound.vue'
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -13,11 +15,13 @@ const router = createRouter({
         { path: '/', redirect: '/teams' },
         {
             name: 'teams',
-            path: '/teams', component: TeamsList, children: [
+            path: '/teams',
+            components: { default: TeamsList, footer: TeamsFooter },
+            children: [
                 { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
             ]
         },
-        { path: '/users', component: UsersList },
+        { path: '/users', components: { default: UsersList, footer: UsersFooter } },
         { path: '/:notFound(.*)', component: NotFound }
     ],
     linkActiveClass: 'active',
