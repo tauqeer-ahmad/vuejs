e803ac6b0ee81edc81b0f4cd1f3496d711aca67f
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-02-23 19:49:56 +0500

    mappers

--- src/components/ChangeCounter.vue
+++ src/components/ChangeCounter.vue
@@ -1,13 +1,18 @@
 <template>
-    <button @click="addOne">Add 1</button>
+    <button @click="inc">Add 1</button>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
     methods: {
-        addOne() {
-            this.$store.dispatch('increment');
-        }
+        // addOne() {
+        //     this.$store.dispatch('increment');
+        // }
+        ...mapActions({
+            inc: 'increment'
+        })
     }
 };
 </script>
