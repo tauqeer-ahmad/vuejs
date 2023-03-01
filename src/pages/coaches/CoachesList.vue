beb516f3e1d871d86e4272eb5ac8ad49e71b2369
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-03-01 12:06:11 +0500

    wiring up pages

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -4,7 +4,11 @@
             FILTER
         </section>
         <section>
-            LIST OF COACHES
+            <div class="controls">
+                <button>Refresh</button>
+                <router-link to="/register">Register as a Coach</router-link>
+            </div>
+            <ul>LIST OF COACHES</ul>
         </section>
     </div>
 </template>
