8532c4b8fd52ef658df27382024d65ffb3bd9d78
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-03-01 16:10:23 +0500

    build coach detail component

--- src/router.js
+++ src/router.js
@@ -13,7 +13,8 @@ const router = createRouter({
         { path: '/', redirect: '/coaches' },
         { path: '/coaches', component: CoachesList },
         {
-            path: '/coaches/:id', component: CoachDetail, children: [
+            path: '/coaches/:id', component: CoachDetail, props: true,
+            children: [
                 { path: 'contact', component: ContactCoach },
             ]
         },
