a38ea160475fcabf7cb348e7bd7bd3853a1e7312
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-03-09 16:45:59 +0500

    adding authentication page (signup and login)

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,95 @@
+<template>
+    <base-card>
+        <form @submit.prevent="submitForm">
+            <div class="form-control">
+                <label for="email">Email</label>
+                <input type="email" id="email" v-model.trim="email">
+            </div>
+            <div class="form-control">
+                <label for="password">Passeord</label>
+                <input type="password" id="password" v-model.trim="password">
+            </div>
+            <p v-if="!formIsValid">Please Enter Email and Password must be 6 characters long.</p>
+            <base-button>{{ submitBtnCaption }}</base-button>
+            <base-button type="button" mode="flat" @click="switchAuthMode">{{ switchModeBtnCaption }}</base-button>
+        </form>
+    </base-card>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            formIsValid: true,
+            mode: 'login'
+        }
+    },
+    computed: {
+        submitBtnCaption() {
+            if (this.mode === 'login') {
+                return 'Login';
+            } else {
+                return 'Signup';
+            }
+        },
+        switchModeBtnCaption() {
+            if (this.mode === 'login') {
+                return 'Signup Instead';
+            } else {
+                return 'Login Instead';
+            }
+        }
+    },
+    methods: {
+        submitForm() {
+            this.formIsValid = true;
+            if (this.email === '' || !this.email.includes('@') || this.password.length < 6) {
+                this.formIsValid = false;
+                return;
+            }
+        },
+        switchAuthMode() {
+            if (this.mode === 'login') {
+                this.mode = 'signup';
+            } else {
+                this.mode = 'login';
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+form {
+    margin: 1rem;
+    padding: 1rem;
+}
+
+.form-control {
+    margin: 0.5rem 0;
+}
+
+label {
+    font-weight: bold;
+    margin-bottom: 0.5rem;
+    display: block;
+}
+
+input,
+textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    border: 1px solid #ccc;
+    padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+    border-color: #3d008d;
+    background-color: #faf6ff;
+    outline: none;
+}
+</style>
