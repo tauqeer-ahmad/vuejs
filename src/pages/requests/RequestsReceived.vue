a5efb7a8949ebf24c5ee549e3b431736ef3f58d9
Tauqeer ahmad <tauqeer.ahmad2008@gmail.com>
2023-03-09 12:31:37 +0500

    sending and fetch coaching http requests

--- src/pages/requests/RequestsReceived.vue
+++ src/pages/requests/RequestsReceived.vue
@@ -1,10 +1,14 @@
 <template>
+    <base-dialog :show="!!error" title="An Error Occured" @close="handleError">
+        <p>{{ error }}</p>
+    </base-dialog>
     <section>
         <base-card>
             <header>
                 <h2>Requests Received</h2>
             </header>
-            <ul v-if="hasRequests">
+            <base-spinner v-if="isLoading"></base-spinner>
+            <ul v-else-if="hasRequests && !isLoading">
                 <request-item v-for="req in receivedRequests" :key="req.id" :email="req.userEmail"
                     :message="req.message"></request-item>
             </ul>
@@ -20,6 +24,12 @@ export default {
     components: {
         RequestItem
     },
+    data() {
+        return {
+            isLoading: false,
+            error: null
+        }
+    },
     computed: {
         receivedRequests() {
             return this.$store.getters['requests/requests'];
@@ -27,6 +37,23 @@ export default {
         hasRequests() {
             return this.$store.getters['requests/hasRequests'];
         }
+    },
+    methods: {
+        async loadRequests() {
+            this.isLoading = true;
+            try {
+                await this.$store.dispatch('requests/fetchRequests');
+            } catch (error) {
+                this.error = error.message || "Something Failed";
+            }
+            this.isLoading = false;
+        },
+        handleError() {
+            this.error = null;
+        }
+    },
+    created() {
+        this.loadRequests();
     }
 };
 </script>
